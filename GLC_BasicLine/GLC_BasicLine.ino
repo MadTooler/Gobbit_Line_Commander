7297d7ffeb9a27205eda267f61c9712d8471e32a
MadTooler <Madtooler@gmail.com>
2016-12-13 05:45:46 -0600

    Add files via upload

---
+++ GLC_BasicLine/GLC_BasicLine.ino
@@ -0,0 +1,17 @@
+#include <GobbitLineCommand.h>
+
+GobbitLineCommand MyBot;
+
+void setup() {
+
+  MyBot.beginGobbit();
+
+  MyBot.calibrateLineSensor(0);
+
+}
+
+void loop() {
+  
+  MyBot.followLine();
+  
+}
