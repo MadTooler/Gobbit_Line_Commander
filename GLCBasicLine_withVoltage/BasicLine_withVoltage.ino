6fdf2739fdb4c02df516886b535aad1c30cd0710
MadTooler <Madtooler@gmail.com>
2016-12-13 05:46:28 -0600

    Add files via upload

---
+++ GLCBasicLine_withVoltage/BasicLine_withVoltage.ino
@@ -0,0 +1,22 @@
+#include <GobbitLineCommand.h>
+
+GobbitLineCommand MyBot;
+
+void setup() {
+
+  MyBot.readBatteryVolts(A5,33,100);
+
+  MyBot.beginGobbit();
+
+  MyBot.calibrateLineSensor(0);
+
+}
+
+void loop() {
+  
+  // check the voltage of the battery
+  MyBot.checkBattery(A5,3.25*2,33,100);
+  
+  MyBot.followLine();
+  
+}
